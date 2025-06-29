e2eec112e0ec35830a8fa371cc6131f9df58735b
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2025-06-29 19:16:54 +0530

    Renamed fields & made extraction module independent

--- src/features/add_feature.py
+++ src/features/add_feature.py
@@ -5,76 +5,95 @@ from utils.zip_handler import ZipHandler
 
 
 def new_files_input(new_files_entry):
-	new_files_path = filedialog.askopenfilenames(
-		title="Select Files to Add",
+	new_files = filedialog.askopenfilenames(
+		title="Select files to add",
 		filetypes=[("All Files", "*.*")]
 	)
 	new_files_path_str = ""
-	for i in range(len(new_files_path)):
+	for i in range(len(new_files)):
 		if i > 0:
 			new_files_path_str += "; "
-		new_files_path_str += new_files_path[i]
+		new_files_path_str += new_files[i]
 	new_files_entry.delete(0, END)
 	new_files_entry.insert(0, new_files_path_str)
 
 
-def existing_zip_input(existing_zip_entry, dest_path_entry):
-	existing_zip_path = filedialog.askopenfilename(
-		title="Select Existing Zip",
+def existing_zip_input(existing_zip_entry, zip_target_entry):
+	existing_zip = filedialog.askopenfilename(
+		title="Select existing zip",
 		filetypes=[("Zip File", "*.zip"), ("Rar File", "*.rar"), ("7z File", "*.7z")]
 	)
 	existing_zip_entry.delete(0, END)
-	existing_zip_entry.insert(0, existing_zip_path)
-	dest_path_entry.delete(0, END)
-	dest_path_entry.insert(0, existing_zip_path)
+	existing_zip_entry.insert(0, existing_zip)
+	zip_target_entry.delete(0, END)
+	zip_target_entry.insert(0, existing_zip)
+
+
+def save_loc_input(save_loc_entry):
+	save_loc = filedialog.askdirectory()
+	save_loc_entry.delete(0, END)
+	save_loc_entry.insert(0, save_loc)
 
 
 def perform_add_operation(entries, show_progress_bar):
-	new_files_path = entries['new_files'].get().strip()
-	existing_zip_path = entries['existing_zip'].get().strip()
-	dest_path = entries['dest_path'].get().strip()
-	password = entries['pass'].get()
+	new_files = entries['new_files'].get().strip()
+	existing_zip = entries['existing_zip'].get().strip()
+	zip_target = entries['zip_target'].get().strip()
+	save_loc = entries['save_loc'].get().strip()
+	pwd = entries['pwd'].get()
 
-	if new_files_path == "":
+	if new_files == "":
 		messagebox.showerror("Required field missing", "Please select files to add.")
-	elif existing_zip_path == "":
+	elif existing_zip == "":
 		messagebox.showerror("Required field missing", "Please select existing zip.")
+	elif save_loc == "":
+		messagebox.showerror("Required field missing", "Please mention location to save modified zip.")
 	else:
 		zip_handler = ZipHandler()
-		if not zip_handler.extract_all_files(existing_zip_path, password):
-			messagebox.showerror("Password required", "Zip is password protected. Please enter correct password.")
+		res = zip_handler.add_files_to_existing_zip(existing_zip, pwd)
+		if res['status']:
+			messagebox.showinfo(res['msg_title'], res['msg_desc'])
+		else:
+			messagebox.showerror(res['msg_title'], res['msg_desc'])
 
 
 def main(parent, show_progress_bar):
 	entries = {}
 
 	# Creating required Widgets.
-	get_label(parent, "Files to Add", False).grid(row=0, column=0, sticky="ew")
+	get_label(parent, "Select files to add", False).grid(row=0, column=0, sticky="ew")
 	new_files_entry_frame, entries['new_files'] = get_entry_frame(parent).values()
 	new_files_entry_frame.grid(row=1, column=0, pady=attr_row_margin, sticky="ew")
 	new_files_browse_btn = get_btn(parent, "Browse")
 	new_files_browse_btn.grid(row=1, column=1, padx=attr_col_margin, pady=attr_row_margin, sticky="ns")
 
-	get_label(parent, "Existing Zip", False).grid(row=2, column=0, sticky="ew")
+	get_label(parent, "Select existing zip", False).grid(row=2, column=0, sticky="ew")
 	existing_zip_entry_frame, entries['existing_zip'] = get_entry_frame(parent).values()
 	existing_zip_entry_frame.grid(row=3, column=0, pady=attr_row_margin, sticky="ew")
 	existing_zip_browse_btn = get_btn(parent, "Browse")
 	existing_zip_browse_btn.grid(row=3, column=1, padx=attr_col_margin, pady=attr_row_margin, sticky="ns")
 
-	get_label(parent, "Destination Folder within Zip").grid(row=4, column=0, sticky="ew")
-	dest_path_entry_frame, entries['dest_path'] = get_entry_frame(parent).values()
-	dest_path_entry_frame.grid(row=5, column=0, pady=attr_row_margin, sticky="ew")
+	get_label(parent, "Target folder within zip").grid(row=4, column=0, sticky="ew")
+	zip_target_entry_frame, entries['zip_target'] = get_entry_frame(parent).values()
+	zip_target_entry_frame.grid(row=5, column=0, pady=attr_row_margin, sticky="ew")
 
 	get_label(parent, "Password").grid(row=6, column=0, sticky="ew")
-	pass_entry_frame, entries['pass'] = get_entry_frame(parent, True).values()
-	pass_entry_frame.grid(row=7, column=0, pady=attr_row_margin, sticky="ew")
+	pwd_entry_frame, entries['pwd'] = get_entry_frame(parent, True).values()
+	pwd_entry_frame.grid(row=7, column=0, pady=attr_row_margin, sticky="ew")
+
+	get_label(parent, "Save modified zip at", False).grid(row=8, column=0, sticky="ew")
+	save_loc_entry_frame, entries['save_loc'] = get_entry_frame(parent).values()
+	save_loc_entry_frame.grid(row=9, column=0, pady=attr_row_margin, sticky="ew")
+	save_loc_browse_btn = get_btn(parent, "Browse")
+	save_loc_browse_btn.grid(row=9, column=1, padx=attr_col_margin, pady=attr_row_margin, sticky="ns")
 
 	add_btn = get_submit_btn(parent, "Add")
-	add_btn.grid(row=8, column=0, pady=15, sticky="w")
+	add_btn.grid(row=10, column=0, pady=15, sticky="w")
 
 	parent.grid_columnconfigure(0, weight=1)
 
 	# Configuring buttons command
 	new_files_browse_btn.config(command=lambda: new_files_input(entries['new_files']))
-	existing_zip_browse_btn.config(command=lambda: existing_zip_input(entries['existing_zip'], entries['dest_path']))
+	existing_zip_browse_btn.config(command=lambda: existing_zip_input(entries['existing_zip'], entries['zip_target']))
+	save_loc_browse_btn.config(command=lambda: save_loc_input(entries['save_loc']))
 	add_btn.config(command=lambda: perform_add_operation(entries, show_progress_bar))
--- src/utils/zip_handler.py
+++ src/utils/zip_handler.py
@@ -5,22 +5,36 @@ import pyzipper
 
 class ZipHandler:
 	def __init__(self):
-		self.__path = os.path.join(os.getcwd(), "zip_operations_temp")
-		self.__extracted_files_path = None
+		self.__path = os.path.join(os.getcwd(), "temp_zip_operations")
 		self.clear()
 		os.mkdir(self.__path)
 
-	def extract_all_files(self, zip_path, password=""):
-		self.__extracted_files_path = os.path.join(self.__path, "extracted_files")
-		os.mkdir(self.__extracted_files_path)
+	@staticmethod
+	def __generate_response(status=True, msg_title=None, msg_desc=None):
+		return {'status': status, 'msg_title': msg_title, 'msg_desc': msg_desc}
+
+	def extract_zip_files(self, src_zip, dest_dir, pwd=""):
 		try:
-			with pyzipper.AESZipFile(zip_path) as zf:
-				if password != "":
-					zf.setpassword(password.encode('utf-8'))
-				zf.extractall(path=self.__extracted_files_path)
-			return True
+			with pyzipper.AESZipFile(src_zip) as zf:
+				if pwd != "":
+					zf.setpassword(pwd.encode('utf-8'))
+				zf.extractall(path=dest_dir)
 		except RuntimeError:
-			return False
+			return self.__generate_response(
+				status=False,
+				msg_title="Password required",
+				msg_desc="Zip is password protected. Please enter correct password."
+			)
+		return self.__generate_response(
+			status=True,
+			msg_title="Files Extraction completed",
+			msg_desc="Files are extracted successfully at {}".format(dest_dir)
+		)
+
+	def add_files_to_existing_zip(self, src_zip, pwd=""):
+		extracted_files_path = os.path.join(self.__path, "extracted_files")
+		res = self.extract_zip_files(src_zip, extracted_files_path, pwd)
+		return res
 
 	def clear(self):
 		if os.path.exists(self.__path):
